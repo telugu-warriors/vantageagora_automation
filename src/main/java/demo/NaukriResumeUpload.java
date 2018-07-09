1286b987e795a8a7ce1e6fd9613b35908aec6780
Sandeep <sandeep55u@gmail.com>
2018-07-09 21:42:10 +0530

    origin/master' updated with pom.xml

--- src/main/java/demo/NaukriResumeUpload.java
+++ src/main/java/demo/NaukriResumeUpload.java
@@ -1,11 +1,13 @@
 package demo;
 
 import java.awt.AWTException;
+
 import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.KeyEvent;
 import java.util.concurrent.TimeUnit;
+
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -16,8 +18,8 @@ public class NaukriResumeUpload{
 
 	public static void main(String[] args) throws AWTException, InterruptedException {
 		// TODO Auto-generated method stub
-		 System.setProperty("webdriver.chrome.driver", ".\\drivers\\chromedriver.exe");
-	        ChromeDriver driver = new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver", ".\\drivers\\chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
 	        driver.manage().window().maximize();
 			driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);	
 			driver.get("https://www.freepdfconvert.com/");
